f8e7a1c7e4b983500ae0c9a1106e579f4b2be961
lukem512 <hi@lukemitchell.co>
2019-07-19 19:04:26 +0100

    Neaten

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require("express")
-var bodyParser = require("body-parser")
-var app = express()
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-var routes = require("./routes/routes.js")(app)
+const routes = require("./routes/routes.js")(app)
 
 app.use(function(req, res){
   res.status(404).send('Page not found')
 });
 
-var server = app.listen(80, function () {
+const server = app.listen(process.env.PORT || 8080, function () {
   console.log("Listening on port %s...", server.address().port)
 })
